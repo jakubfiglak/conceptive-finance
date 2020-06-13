5323c87897f8bf7f01fbf9fb94b265c996fdf12e
Jakub Figlak <jakub.figlak@gmail.com>
2020-06-13 17:18:40 +0200

    Add favicon

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
+import favicon from '../assets/icons/favicon.ico';
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
@@ -66,7 +67,9 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <link rel="icon" href={favicon} />
+    </Helmet>
   );
 }
 
